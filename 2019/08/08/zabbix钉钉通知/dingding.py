5fd617779435920913dbc38526ad448c0b39dd0e
kxinter <kxhuanzi@163.com>
2019-09-18 10:01:57 +0800

    Site updated: 2019-09-18 10:01:39

--- 2019/08/08/zabbix钉钉通知/dingding.py
+++ 2019/08/08/zabbix钉钉通知/dingding.py
@@ -7,7 +7,7 @@ import sys
 import os
  
 headers = {'Content-Type': 'application/json;charset=utf-8'}
-api_url = "https://oapi.dingtalk.com/robot/send?access_token=92bfa50db10658ee1c37f9ab6b7a000e14ce94a901002170762e472af3754fbf"
+api_url = "https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
  
 def msg(text):
     json_text= {
